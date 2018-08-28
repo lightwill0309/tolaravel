64534913aaff00a934b47a7d888696f3459e8463
root <root@ip-172-31-46-105.ap-northeast-1.compute.internal>
2018-08-28 08:48:32 +0000

    index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <title>tolaravel</title>
+<style>
+#titleArea {
+  background-color:#1ab;
+  color:#fff;
+}
+</style>
+</head>
+<body>
+<div id="titleArea">
+  <h1>tolaravel</h1>
+</div>
+</body>
+</html>
