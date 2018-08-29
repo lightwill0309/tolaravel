992ac44907e1c9bc75001f086bec700713e77900
lightwill0309 <brilliantwish54@gmail.com>
2018-08-29 15:06:29 +0900

    add contents

--- index.php
+++ index.php
@@ -6,11 +6,24 @@
   background-color:#1ab;
   color:#fff;
 }
+.tour {
+  display:inline-block;
+  background-color:#bee;
+  width:300px;
+  height:160px;
+  text-align:center;
+  line-height:160px;
+  color:#fff;
+  font-size:40px;
+}
 </style>
 </head>
 <body>
 <div id="titleArea">
   <h1>tolaravel</h1>
 </div>
+<div class="tour">東京</div>
+<div class="tour">パリ</div>
+<div class="tour">ローマ</div>
 </body>
 </html>
