901d25ff6d8cf0c9c142a88394d5707fc030ffe9
lightwill0309 <brilliantwish54@gmail.com>
2018-08-29 09:42:19 +0000

    contents new

--- index.php
+++ index.php
@@ -16,14 +16,27 @@
   color:#fff;
   font-size:40px;
 }
+.tourSea .tour {
+  background-color:#1ec1e1;
+}
+.tourGroup {
+  margin-bottom:5px;
+}
 </style>
 </head>
 <body>
 <div id="titleArea">
   <h1>tolaravel</h1>
 </div>
+<div class="tourGroup tourCity">
 <div class="tour">東京</div>
 <div class="tour">パリ</div>
 <div class="tour">ローマ</div>
+</div>
+<div class="tourGroup tourSea">
+<div class="tour">ハワイ</div>
+<div class="tour">グアム</div>
+<div class="tour">ドバイ</div>
+</div>
 </body>
 </html>
